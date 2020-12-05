58215678f866098898f828529faa9714e350f7a0
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2020-12-05 18:11:34 +0200

    first commit

---
+++ WpfApp1/Color.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+
+    public class Color_rgb : INotifyPropertyChanged
+    {
+
+        private Color currentColor = new Color();
+        private byte a = 0;
+        private byte r = 0;
+        private byte g = 0;
+        private byte b = 0;
+
+        public Color CurrentColor
+        {
+
+            get => currentColor;
+            set
+            {
+                currentColor = value;
+                OnPropertyChanged();
+            }
+        }
+        public Color ColorInBorder => Color.FromArgb(A, R, G, B);
+        public byte A
+        {
+            get => a;
+            set
+            {
+                a = Convert.ToByte(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ColorInBorder));
+            }
+        }
+        public byte R
+        {
+            get => r;
+            set
+            {
+                r = Convert.ToByte(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ColorInBorder));
+            }
+        }
+        public byte G
+        {
+            get => g;
+            set
+            {
+                g = Convert.ToByte(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ColorInBorder));
+            }
+        }
+        public byte B
+        {
+            get => b;
+            set
+            {
+                b = Convert.ToByte(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ColorInBorder));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        }
+
+    }
+
+}
---
+++ WpfApp1/ViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows.Media;
+using WpfApp1.Commands;
+
+namespace WpfApp1
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Color currentColor;
+        private byte a = 100;
+        private byte r = 150;
+        private byte g = 75;
+        private byte b = 33;
+        private readonly ICollection<Color> colors = new ObservableCollection<Color>();
+
+        private Command addNewColorCommand;
+        private Command removeContactCommand;
+        public ViewModel()
+        {
+            addNewColorCommand = new DelegateCommand(AddColorCommand);
+
+            PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == nameof(CurrentColor))
+                {
+                    addNewColorCommand.RaiseCanExecuteChanged();
+                }
+            };
+        }
+
+        public IEnumerable<Color> Colors => colors;
+        public Color CurrentColor
+        {
+            get => currentColor;
+            set
+            {
+                currentColor = value;
+                OnPropertyChanged();
+            }
+        }
+        public Color ColorInBorder => Color.FromArgb(A, R, G, B);
+        public byte A
+        {
+            get => a;
+            set
+            {
+                a = Convert.ToByte(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ColorInBorder));
+            }
+        }
+        public byte R
+        {
+            get => r;
+            set
+            {
+                r = Convert.ToByte(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ColorInBorder));
+            }
+        }
+        public byte G
+        {
+            get => g;
+            set
+            {
+                g = Convert.ToByte(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ColorInBorder));
+            }
+        }
+        public byte B
+        {
+            get => b;
+            set
+            {
+                b = Convert.ToByte(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ColorInBorder));
+            }
+        }
+
+        public ICommand AddNewColorCommand => addNewColorCommand;
+
+        public void AddColorCommand()
+        {
+            colors.Add(ColorInBorder);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
